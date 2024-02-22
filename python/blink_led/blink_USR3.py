c72be3872d6083455e2e7058d2481445407f385d
Demo User <debian@beaglebone.localdomain>
2024-02-22 22:55:09 +0000

    added new directory "blink_LED", added .py file that blinks USR3 LED 5 times in one second

---
+++ python/blink_led/blink_USR3.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+--------------------------------------------------------------------------
+Blink USR3 LED
+--------------------------------------------------------------------------
+License:   
+Copyright 2024 - Amelia Pillar
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Uses the Adafruit BBIO Library to blink the USR3 at 5 Hz (5 blinks/second)
+
+--------------------------------------------------------------------------
+"""
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+#sets user LED numbers
+for i in range(4):
+    GPIO.setup("USR%d" % i, GPIO.OUT)
+
+#blinks USR3 LED on and off 5 times in one second
+for j in range(5):
+        #stays on for .1 second
+        GPIO.output("USR%d" % 3, GPIO.HIGH)
+        time.sleep(.1)
+        #stays off for .1 second
+        GPIO.output("USR%d" % 3, GPIO.LOW)
+        time.sleep(.1)
