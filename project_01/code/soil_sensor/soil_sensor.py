920927ce7e979aada87c723a733c132bcd2dae98
Demo User <debian@beaglebone.localdomain>
2024-04-10 00:47:41 +0000

    added more to soil sensor code

--- project_01/code/soil_sensor/soil_sensor.py
+++ project_01/code/soil_sensor/soil_sensor.py
@@ -1,4 +1,5 @@
 #from https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/python-circuitpython-test
+#also from https://www.hackster.io/sagrawal96/plant-babysitter-a85198
 
 import time
 import board
@@ -9,12 +10,38 @@ i2c_bus = board.I2C()  # uses board.SCL and board.SDA
 
 ss = Seesaw(i2c_bus, addr=0x36)
 
-while True:
-    # read moisture level through capacitive touch pad
+
+def turn_off_moisture_sensor():
+    """Turn off the moisture sensor 
+    """
+    gpio_set(GPIO_VAL, LOW)
+# End def
+
+
+def turn_on_moisture_sensor():
+    """Turn on the moisture sensor
+    """
+    gpio_set(GPIO_VAL, HIGH)
+# End def
+
+
+def setup_sensor():
+    """Setup all the pins for the reading.
+    """
+    gpio_setup(GPIO_VAL, OUT, LOW)
+#End def
+
+
+def read_moisture():
+    """ set up stuff and read moisture level"""
+   
     touch = ss.moisture_read()
-
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
-
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
-    time.sleep(1)
+    if touch < 580:
+        water = "low"
+    else:
+        water = "okay"
+    print("  moisture: " + str(water))
+    
+    return(water)
+    
+#end def
